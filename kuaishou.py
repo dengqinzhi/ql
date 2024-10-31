b30866f23af996e9569fa3dea797846c777b947a
dengqinzhi <toneydeng@126.com>
2024-10-31 15:19:36 +0800

    Create kuaishou.py
    快手极速版

---
+++ kuaishou.py
@@ -0,0 +1,8 @@
+# -*- coding=UTF-8 -*-
+# @Project          QL_TimingScript
+# @fileName         快手极速版.py
+# @author           Echo
+# @EditTime         2024/9/26
+from typing import *
+
+cookie = "kuaishou.api_st=Cg9rdWFpc2hvdS5hcGkuc3QSoAGdayy5Dc4sfKoFP-URnKPaLCli7TB63CK3hCz5w0wUR_rEHFlGPQe3z1y83fGlrs483oOMqArNbuzEqElo9cEATu1YuX_xf3lKL7zuY1rkBR8NNqVwF9la74aJVU28o3qIdbIOc9sDqS-hTCHJNLHYVnsCQVV1A7I5bZe6uY1e0IIzsigLpQONNxZG16zOuDouPbxNkGLjPU2a4R1sbFnTGhJThZNu-rRPH4Mw3KnOATqUKJgiIBIQzfWyyV5b2R6l4UQdbPmigu9kNTaO-FDruFMwaMk8KAUwAQ;"
